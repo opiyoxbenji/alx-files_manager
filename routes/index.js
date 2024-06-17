869928f8e915708a2ef0638d7348bd5bb4f6ed4f
pixelbushi <opiyosbenji@gmail.com>
2024-06-17 16:14:15 +0300

    added new endpoints to the router/index.js file

--- routes/index.js
+++ routes/index.js
@@ -12,5 +12,10 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.get('/files/:id', FilesController.getShow)
+router.get('/files', FilesController.getIndex)
+router.put('files/:id/publish', FilesController.putPublish)
+router.put('/files/:id/publish', FilesController.putUnpublish)
+router.get('/files/:id/data', FilesController.getFile)
 
 module.exports = router;
