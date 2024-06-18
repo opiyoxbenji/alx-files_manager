45b6b886f9e67e63c52f92714a2d4894bb29b046
JAssocc <clnearle@gmail.com>
2024-06-18 08:55:41 -0700

    index update

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.post('/files', FilesController.postUpload);
 
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
-router.put('files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/publish', FilesController.putUnpublish);
 // router.get('/files/:id/data', FilesController.getFile);
 
