117b57ce677da44a2eb2525808294f7aed29ce90
JAssocc <clnearle@gmail.com>
2024-06-18 04:29:51 -0700

    task 7 update

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,8 @@ router.post('/files', FilesController.postUpload);
 
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
-// router.put('files/:id/publish', FilesController.putPublish);
-// router.put('/files/:id/publish', FilesController.putUnpublish);
+router.put('files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/publish', FilesController.putUnpublish);
 // router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
