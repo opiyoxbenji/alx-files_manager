a3505926ea646a5fe78ee856758b440c626ccbfc
JAssocc <clnearle@gmail.com>
2024-06-19 08:32:06 -0700

    fix

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -240,10 +240,16 @@ class FilesController {
 
   static async getFile(req, res) {
     const fileId = req.params.id;
+    if (!fileId) {
+      return res.status(404).json({ error: 'Not found' });
+    }
     const token = req.headers['x-token'] || null;
     try {
       const key = `auth_${token}`;
       const userId = token ? await redisClient.get(key) : null;
+      if (!userId) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
       const { db } = dbClient;
       const filesCollection = db.collection('files');
       const file = await filesCollection.findOne({ _id: ObjectId(fileId) });
@@ -256,13 +262,13 @@ class FilesController {
       if (!file.isPublic) {
         if (!token || !userId || userId !== file.userId.toString()) {
           return res.status(404).json({ error: 'Not found' });
-	}
+        }
       }
       if (!fs.existsSync(file.localPath)) {
         return res.status(404).json({ error: 'Not found' });
       }
       const fileData = fs.readFileSync(file.localPath);
-      const mimeType = mime.lookup(file.name) || 'application/octet-stream';;
+      const mimeType = mime.lookup(file.name) || 'application/octet-stream';
       res.setHeader('Content-Type', mimeType);
       return res.status(200).send(fileData);
     } catch (error) {
